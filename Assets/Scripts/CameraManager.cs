d3cd30c8d2f93c1ea84ede10f1e41c69ec330dda
kangtoe <kangtoe99@gmail.com>
2023-07-22 23:40:01 +0900

    카메라 흔들림 구현

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -11,10 +11,14 @@ public class CameraManager : MonoBehaviour
     [SerializeField]
     Vector3 offset = new Vector3(0, 0, 2.5f);
 
+    Vector3 originPos;
+
     // Start is called before the first frame update
     void Start()
     {
         chaseTarget = Player.Instance.transform;
+
+        originPos = Camera.main.transform.localPosition;
     }
 
     // Update is called once per frame
@@ -22,9 +26,36 @@ public class CameraManager : MonoBehaviour
     {
         Vector3 pos = chaseTarget.position;
         
-        // z√‡¿∏∑Œ∏∏ √ﬂ¿˚«‘
+        // zÏ∂ïÏúºÎ°úÎßå Ï∂îÏ†ÅÌï®
         Vector3 camCenter = new Vector3(0, 0, pos.z);
                
         transform.position = camCenter + offset;
+    }
+
+    public void ShakeDebug()
+    {
+        Skake(0.1f, 0.2f);
+    }
+
+    public void Skake(float _amount, float _duration)
+    {
+        //Debug.Log("Skake");
+        StopAllCoroutines();
+        StartCoroutine(ShakeCr(_amount, _duration));
+    }
+
+    IEnumerator ShakeCr(float _amount, float _duration)
+    {
+        float timer = 0;
+        while (timer <= _duration)
+        {
+            Camera.main.transform.localPosition = (Vector3)Random.insideUnitCircle * _amount + originPos;
+
+            timer += Time.deltaTime;
+            //yield return new WaitForSeconds(0.1f);
+            yield return null;
+        }
+        Camera.main.transform.localPosition = originPos;
+
     }
 }
