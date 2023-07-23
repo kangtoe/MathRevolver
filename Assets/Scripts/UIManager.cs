6a147264f39390a908f59af9ab7ddbaf3087d79c
kangtoe <kangtoe99@gmail.com>
2023-07-23 09:53:18 +0900

    UiManager -> 화면 점멸 효과 기능을 추가 구현

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -17,11 +18,20 @@ public class UIManager : MonoBehaviour
     }
     private static UIManager instance;
 
+    [Header("옵션")]
     [SerializeField]
     GameObject optionUI;
-
     public static bool isOnUI;
     float timeScaleOnMenu = 0.5f;
+
+    [Header("화면 점멸")]
+    [SerializeField]
+    Image flashPanel;
+    [SerializeField]
+    float flashDuration = 0.5f;
+    [SerializeField]
+    float flashMaxAlpha = 0.5f;
+    Coroutine m_FlashCr;
 
     // Start is called before the first frame update
     void Start()
@@ -47,6 +57,32 @@ public class UIManager : MonoBehaviour
         isOnUI = false;
         optionUI.SetActive(false);
         Time.timeScale = 1;
+    }
+
+    public void Flash()
+    {
+        if (m_FlashCr != null) StopCoroutine(FlashCr());
+        m_FlashCr = StartCoroutine(FlashCr());
+    }
+
+    IEnumerator FlashCr()
+    {
+        float t = 0;
+        Color color = flashPanel.color;
+
+        while (true)
+        {
+            color.a = Mathf.Lerp(flashMaxAlpha, 0, t);
+            flashPanel.color = color;
+
+            t += Time.deltaTime / flashDuration;
+
+
+
+            if (t >= 1) break;
+
+            yield return null;
+        }
     }
 
     public void QuitGame()
