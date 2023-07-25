5370f3df158c1497606bb586f3808df746995d76
kangtoe <kangtoe99@gmail.com>
2023-07-25 17:34:19 +0900

    타이틀 화면 캔버스 해상도 수정, 진단평가 Scene 빌드 씬에 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,5 +14,12 @@ public class GameManager : MonoBehaviour
     void Init()
     {
         Application.targetFrameRate = 60;
+
+        int setWidth = 1920; // 화면 너비
+        int setHeight = 1080; // 화면 높이
+
+        //해상도를 설정값에 따라 변경
+        //3번째 파라미터는 풀스크린 모드를 설정 > true : 풀스크린, false : 창모드
+        //Screen.SetResolution(setWidth, setHeight, true);
     }
 }
