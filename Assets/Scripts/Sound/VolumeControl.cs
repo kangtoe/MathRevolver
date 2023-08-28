155a3d0f90ee1a0f3f04773c771050b9b1d451fe
kangtoe <kangtoe99@gmail.com>
2023-08-28 14:00:15 +0900

    볼룸 조절 기반 작업

--- Assets/Scripts/Sound/VolumeControl.cs
+++ Assets/Scripts/Sound/VolumeControl.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 // 볼륨 제어
 public class VolumeControl : MonoBehaviour
 {
-    private void OnEnable()
-    {
-        // bgmVolume = 실제 볼륨
-        // sfxVolume = 실제 볼륨
-    }
+    [SerializeField]
+    AudioMixer audioMixer;
+
+    [SerializeField]
+    float min_dB = -40;
+    [SerializeField]
+    float max_dB = 0;
+
+    [SerializeField]
+    float currentVolume_dB_bgm;
+    [SerializeField]
+    float currentVolume_dB_sfx;
 
     [Range(0, 1)]
     [SerializeField]
@@ -23,7 +31,7 @@ public class VolumeControl : MonoBehaviour
         set
         {
             bgmVolume = value;
-            // 실제 볼륨 제어
+            UpdateMixer();
         }
     }
 
@@ -38,8 +46,38 @@ public class VolumeControl : MonoBehaviour
         }
         set
         {
-            sfxVolume = value;
-            // 실제 볼륨 제어
+            sfxVolume = value;            
+            UpdateMixer();
         }
+    }
+
+    private void Start()
+    {
+        BgmVolume = SaveManager.BgmVolume;
+        SfxVolume = SaveManager.SfxVolume;
+    }
+
+    private void OnValidate()
+    {
+        UpdateMixer();
+    }
+
+    // 실제 볼륨 제어
+    void UpdateMixer()
+    {
+        currentVolume_dB_bgm = Get_dB(bgmVolume);
+        audioMixer.SetFloat("BGM", currentVolume_dB_bgm);
+
+        currentVolume_dB_sfx = Get_dB(sfxVolume);
+        audioMixer.SetFloat("SFX", currentVolume_dB_sfx);
+    }
+
+    float Get_dB(float volume)
+    {
+        float dB;
+        if (volume == 0) dB = - 80; // 완전 음소거
+        else dB = Mathf.Lerp(min_dB, max_dB, volume);
+
+        return dB;
     }
 }
