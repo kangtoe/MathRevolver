d6d19cffbd0d8e67e4ce8f615add81339c8c28b0
kangtoe <kangtoe99@gmail.com>
2023-08-31 06:04:51 +0900

    반복해서 흐르는 이미지 구현, 보스 경고 이미지에 적용 및 프리팹화

---
+++ Assets/Scripts/FlowImage.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// 반복해서 흘러가는 이미지
+
+// 이미지 소스 스프라이트 설정 : wrap mode = repeat
+// 이미지의 메터리얼 설정 : UI/Shader/Unlit/Transparent
+// 이미지 소스 스프라이트를 메터리얼의 texture로도 사용할 것
+
+public class FlowImage : MonoBehaviour
+{
+    Image image;
+    Material InstancedMaterial;
+
+    [SerializeField]
+    float Speed = 0.1f;    
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+        InstancedMaterial = image.material;                
+    }
+
+    private void Update()
+    {
+        InstancedMaterial.mainTextureOffset += new Vector2(Speed * Time.deltaTime, 0);
+    }
+}
