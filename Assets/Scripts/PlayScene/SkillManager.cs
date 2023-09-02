716c9a8ae9ead1d9dd25c3dab0e6f24ee4c2294a
suldangoo <suldangoo@gmail.com>
2023-09-02 12:36:54 +0900

    최종 밸런싱 2차

--- Assets/Scripts/PlayScene/SkillManager.cs
+++ Assets/Scripts/PlayScene/SkillManager.cs
@@ -111,7 +111,7 @@ public class SkillManager : MonoBehaviour
 
                 state = SkillState.Flash;
                 fill.sprite = flash;
-                yield return new WaitForSeconds(0.1f);
+                yield return new WaitForSeconds(0.15f);
 
                 state = SkillState.Ready;
                 fill.sprite = loaded;
